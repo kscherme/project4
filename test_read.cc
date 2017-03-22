4dc4b06357ccce3aecf1902b7d48e45d479b638f
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-03-21 20:09:57 -0400

    everything but threads

--- test_read.cc
+++ test_read.cc
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <curl/curl.h>
+#include <fstream>
 #include "Config.h"
 #include "Parse.h"
 #include "CurlSite.h"
@@ -28,11 +29,34 @@ int main() {
 	Parse psites(config.SITE_FILE, config.NUM_PARSE);
 	vector<string> sites = psites.getData();
 
+	// Make output file
+	int fileCount = 1;
+	ofstream outputFile;
+	string filename = to_string(fileCount) + ".csv";
+	outputFile.open (filename);
+
+
 	// Get website contents
 	const char* c = sites.front().c_str();
 	CurlSite curl;
-	char* data = curl.getSite(c);
-	cout << data << endl;
+	const char* data = curl.getSite(c);
+	string data1 = string(data);
+	cout << data1 << endl;
+
+	// Perform keyword search on data
+	for (vector<string>::iterator it = keywords.begin(); it != keywords.end(); ++it) {
+		int count = 0;
+		size_t nPos = data1.find(*it, 0);
+		while(nPos != string::npos) {
+			count++;
+			nPos = data1.find(*it, nPos + 1);
+		}
+		cout << *it << ": " << count << endl;
+		outputFile << *it << ": " << count << endl;
+
+	}
+
+	outputFile.close();
 
 	return 0;
 
